3f3dfaf34476a24da750772fb6e2a0437f7322fd
Chadni Jahan Liza <chadnijahanliza@gmail.com>
2026-01-20 20:19:15 +0600

    Update java.java

--- java.java
+++ java.java
@@ -1,17 +1,22 @@
-
 <application
-        android:allowBackup="true"
-        android:dataExtractionRules="@xml/data_extraction_rules"
-        android:fullBackupContent="@xml/backup_rules"
-        android:icon="@mipmap/ic_launcher"
-        android:label="@string/app_name"
-        android:roundIcon="@mipmap/ic_launcher_round"
-        android:supportsRtl="true"
-        android:theme="@style/Theme.SpokenEnglish">
+    android:name=".MyApp"
+    android:allowBackup="true"
+    android:dataExtractionRules="@xml/data_extraction_rules"
+    android:fullBackupContent="@xml/backup_rules"
+    android:icon="@mipmap/ic_launcher"
+    android:label="@string/app_name"
+    android:roundIcon="@mipmap/ic_launcher_round"
+    android:supportsRtl="true"
+    android:theme="@style/Theme.SpokenEnglish">
 
-        <activity
-            android:name=".Conversation_Eight_Activity"
-            android:exported="false" />
-        <activity
-            android:name=".Conversation_Seven_Activity"
-            android:exported="false" /
+    <activity
+        android:name=".Conversation_Eight_Activity"
+        android:exported="false" />
+
+    <activity
+        android:name=".Conversation_Seven_Activity"
+        android:exported="false" />
+
+    <!-- অন্য সব Activity এখানেই থাকবে -->
+
+</application>
